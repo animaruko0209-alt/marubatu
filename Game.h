596222c265cfe8bfe7aead1d711c23b69c4a81a9
4A-22\koyo <a>
2026-09-05 10:08:34 +0900

    WinMain　ひな形追加 Game　ゲームループ追加 SceneBase　追加

--- Game.h
+++ Game.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <memory>
+#include"Dxlib.h"
+class Game
+{
+	int scene_no = 0;
+
+	std::shared_ptr<SceneBase> current_scene_ptr = nullptr;
+
+
+public:
+
+	Game() {}
+
+	void Game_loop();
+
+};
